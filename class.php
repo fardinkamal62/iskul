a997c846df26368e4cecd940e3e15465d1bdd7a9
Fardin Kamal <70953546+fardinkamal62@users.noreply.github.com>
2022-02-27 19:53:34 +0600

    Fixing

--- class.php
+++ class.php
@@ -1,10 +1,78 @@
 <?php
-/*
-$previous = "javascript:history.go(-1)";
-if(isset($_SERVER['HTTP_REFERER'])) {
-    $previous = $_SERVER['HTTP_REFERER'];
+include("config.php");
+
+if (isset($_COOKIE['lin'])) {
+    echo("<a href='dashboard.php'>Dashboard</a><br><br>");
+    date_default_timezone_set("Asia/Dhaka");
+    $t=time();
+    echo("Current time: "); echo(date("h:i a",$t));
+
+    $sql = "SELECT * FROM `sched` WHERE day = 1";
+    $result = mysqli_query($con, $sql);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+    $first = $row['first'];
+    $sec = $row['second'];
+    $third = $row['third'];
 }
-echo "<a href=' $previous'>Back</a>";
-*/
-echo "<script>javascript:history.go(-1)</script>";
+else {
+    header("Location: signin.php");
+}
+
+echo "<br>";
+
+$day = 0;
+
+switch (date("l")) {
+    case 'Friday':
+        $id = 0;
+        break;
+    case 'Saturday':
+        $id = 1;
+        break;
+    case 'Sunday':
+        $id = 2;
+        break;
+    case 'Monday':
+        $id = 3;
+        break;
+    case 'Tuesday':
+        $id = 4;
+        break;
+    case 'Wednesday':
+        $id = 5;
+        break;
+    case 'Thursday':
+        $id = 6;
+        break;
+}
+
+if ($id = 0) {
+    echo "No classes today";
+}
+else {
+    echo <<<EOL
+    <html>
+        <!DOCTYPE html>
+        <br>
+        <body>
+            <h3>Today's schedule:</h3>
+                <table border = "1">
+                <tr>
+                    <th>8-8:40</th>
+                    <th>8:50-9:30</th>
+                    <th>9:40-10:20</th>
+                </tr>
+                <tr>
+                    <td>$first </td>
+                    <td>$sec </td>
+                    <td>$third </td>
+                </tr>
+                </table>
+    
+        </body>
+    </html>
+EOL;
+}
+
 ?>
--- dashboard.php
+++ dashboard.php
@@ -1,12 +1,14 @@
 <?php
 include("config.php");
+date_default_timezone_set("Asia/Dhaka");
+$t=time();
 if(isset($_COOKIE['lin'])){
     $lin = $_COOKIE['lin'];
-
+  
     $sql = "SELECT * FROM users WHERE lin = '$lin'";
     $result = mysqli_query($con, $sql);
     $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-    
+
     if(empty($row['name'])){header("Location: logout.php");}; // auto logout if logged in from another device
     
     $name = $row['name'];
@@ -16,48 +18,48 @@ if(isset($_COOKIE['lin'])){
 else{
     echo "You're not signed in, please <a href='signin.php'> sign in </a></p>";
   }
+  echo "<br>";
+  echo("Current time: "); echo(date("h:i a",$t));
 ?>
 <!DOCTYPE html>
 <html>
+<link rel="stylesheet" href="index.css">
 <?php
 if(isset($_COOKIE['lin'])){
   echo <<<EOL
-  <h1>Hello $name,</h1>
+  <h1>Hello $name</h1>
   <p>Hope you're doing well.</p>
-  <p>Have a nice day😊</p>
   <a href='class.php'>Class</a>
-  <hr>
+  <br>
+  <a href='exam.php'>Exam</a>
   </html>
 
 EOL;
-if ($row['desig'] == 'stud') 
-{
-  echo "<a href='exam.php'>Exam</a>";
-}
 
 if ($row['desig'] == 'gteach' | $row['desig'] == 'princ') 
 {
-$sroll = $row['sroll'];
-$eroll = $row['eroll'];
-$x = $sroll;
-while ($x <= $eroll)
-{
-  $present = "SELECT present,name FROM `users` WHERE roll = $x";
-  $result = mysqli_query($con, $present);
-  $prow = mysqli_fetch_array($result, MYSQLI_ASSOC);
-  $naem = $prow['name'];
-  if ($prow['present'] == 0) 
+  $sroll = $row['sroll'];
+  $eroll = $row['eroll'];
+  $x = $sroll;
+  echo "<hr><h2><u>Attendance</u></h2>";
+  while ($x <= $eroll)
   {
-    echo "<p style='color: red;'>ID:$x $naem</p>";
-  }
-  else 
-  {
-    echo "<p style='color: green;'>ID: $x $naem</p>";
-  }
-
-  echo '<br>';
-  $x++;
+    $present = "SELECT present,name FROM `users` WHERE roll = $x";
+    $result = mysqli_query($con, $present);
+    $prow = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $naem = $prow['name'];
+    if ($prow['present'] == 0) 
+    {
+      echo "<p style='color: red;'>ID:$x $naem</p>";
     }
+    else 
+    {
+      echo "<p style='color: green;'>ID: $x $naem</p>";
+    }
+
+    echo '<br>';
+    $x++;
   }
+}
 }
 ?>
